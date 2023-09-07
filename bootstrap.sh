05c0c029878a3248ebbc20c6569dd6e33d27ea4c
NathanDjianMartin <12380129+NathanDjianMartin@users.noreply.github.com>
2023-09-07 09:09:37 +0200

    fix(bootstrap): add brew to PATH after bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,8 @@ export DOTFILES=$HOME/.dotfiles
 if test ! $(which brew)
 then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Install Brewfile dependencies
